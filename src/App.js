11e57c4da856a74bcc4da9582f63c9c10b6d0225
bfritz0815 <bfritz0815@googlemail.com>
2023-10-06 15:57:16 +0200

    n.a. WIP started on timetravel

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ function Square ({value, onSquareClick}) {
   </button>;
 }
 
-export default function Board() {
-
-  let [squares, setSquares] = useState(Array(9).fill(null));
-  let [xIsNext, setXIsNext] = useState(true);
+function Board({xIsNext, squares, onPlay}) {
 
   const winner = calculateWinner(squares);
   let status;
@@ -27,8 +24,7 @@ export default function Board() {
     const nextSquares = squares.slice();
 
     nextSquares[id] = xIsNext? "X": "O";
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   return <>
@@ -49,6 +45,29 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={()=>handleSquareClick(8)}></Square>
     </div>
   </>;
+}
+
+export default function Game() {
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares){
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}></Board>
+        </div>
+        <div className="game-info">
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+  );
 }
 
 function calculateWinner(squares) {
