97b049bbce904a046ab8f8fb35e12132329c78ec
bfritz0815 <bfritz0815@googlemail.com>
2023-10-06 15:08:37 +0200

    n.a. central data model

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import {useState} from "react";
 
-function Square ({id}) {
-  let [value, setValue] = useState(null);
-  function handleClick(){
-    setValue("X");
-    console.log("click: ",id," value: ",value);
-  }
+function Square ({value, onSquareClick}) {
+
 
   return <button className="square"
-                 onClick={handleClick}>
+                 onClick={onSquareClick}>
     {value}
   </button>;
 }
 
 export default function Board() {
+
+  let [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleSquareClick(id){
+    const nextSquares = squares.slice();
+    nextSquares[id] = "X";
+    setSquares(nextSquares);
+  }
+
   return <>
     <div className="board-row">
-      <Square id={1}></Square>
-      <Square id={2}></Square>
-      <Square id={3}></Square>
+      <Square value={squares[0]} onSquareClick={()=>handleSquareClick(0)}></Square>
+      <Square value={squares[1]} onSquareClick={()=>handleSquareClick(1)}></Square>
+      <Square value={squares[2]} onSquareClick={()=>handleSquareClick(2)}></Square>
     </div>
     <div className="board-row">
-      <Square id={4}></Square>
-      <Square id={5}></Square>
-      <Square id={6}></Square>
+      <Square value={squares[3]} onSquareClick={()=>handleSquareClick(3)}></Square>
+      <Square value={squares[4]} onSquareClick={()=>handleSquareClick(4)}></Square>
+      <Square value={squares[5]} onSquareClick={()=>handleSquareClick(5)}></Square>
     </div>
     <div className="board-row">
-      <Square id={7}></Square>
-      <Square id={8}></Square>
-      <Square id={9}></Square>
+      <Square value={squares[6]} onSquareClick={()=>handleSquareClick(6)}></Square>
+      <Square value={squares[7]} onSquareClick={()=>handleSquareClick(7)}></Square>
+      <Square value={squares[8]} onSquareClick={()=>handleSquareClick(8)}></Square>
     </div>
   </>;
 }
