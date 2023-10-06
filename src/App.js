bb235f2ffb589b09326d0ae62c69308a873a9bd1
bfritz0815 <bfritz0815@googlemail.com>
2023-10-06 15:50:07 +0200

    n.a. full game

--- src/App.js
+++ src/App.js
@@ -12,14 +12,27 @@ function Square ({value, onSquareClick}) {
 export default function Board() {
 
   let [squares, setSquares] = useState(Array(9).fill(null));
+  let [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   function handleSquareClick(id){
+    if (squares[id] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
-    nextSquares[id] = "X";
+
+    nextSquares[id] = xIsNext? "X": "O";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return <>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={()=>handleSquareClick(0)}></Square>
       <Square value={squares[1]} onSquareClick={()=>handleSquareClick(1)}></Square>
@@ -36,4 +49,24 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={()=>handleSquareClick(8)}></Square>
     </div>
   </>;
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
